17a114b896e55fa2ce5b894920769afbb5261c77
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-03-07 21:18:18 +0530

    Add files via upload

---
+++ day1-dsa.js
@@ -0,0 +1,14 @@
+// // let a = 10;
+// // let b = 20;
+// // console.log("sum of "+ a +" and " +  b + " is " + a+b);
+
+// // Accept and print the answer?
+// let age = Number (prompt("Enter Your age "));
+// console.log(age);
+
+
+// swap two variables via 3 method 
+let a = 10;
+let b = 20;
+console.log([a,b]= [b,a]);
+ 
