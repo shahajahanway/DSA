9b2ab8989ac15fbcac16148991c96b0a8459d963
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-03-15 13:00:38 +0530

    Add files via upload

---
+++ Day 2/script.js
@@ -0,0 +1,78 @@
+// Generate OTP ?
+// console.log(Math.floor(Math.random()*900000 + 100000));
+
+console.log("CalCulate Compound interest");
+// CalCulate Compound interest ?
+// let p = Number(prompt("Enter a price"))
+// let r = Number(prompt("Enter a rate"))
+// let t = Number(prompt("Enter a time "))
+
+// console.log(( p * Math.pow ( 1+ r/100, t)) - p );
+
+console.log("Area of triangle of heron's formula");
+
+// Area of triangle of heron's formula
+// let a = Number(prompt("Enter a number"))
+// let b = Number(prompt("Enter a 2nd number"))
+// let c = Number(prompt("Enter a 3td number"))
+
+// if (a+b <= c || a+c <= b || b+c <= a ) {
+//     console.log("Not Possiable");
+    
+// }else{
+//     let s = (a+b+c)/2
+//     console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+    
+// }
+
+console.log("circumference of circle?");
+// circumference of circle?
+// let r = Number(prompt("Enter a number"))
+// console.log(2*Math.PI * r);
+
+
+console.log("If- Else");
+// If - Else
+// if(10>37){
+//     console.log("hello world");
+// }else if(18>5){
+//     console.log("world");
+// }else{
+//     console.log("Hello Worlds ways");
+    
+// }
+console.log("Accept two number and print the greates between them?");
+// Accept two number and print the greates between them?
+// let a = Number(prompt("Enter a Number"))
+// let b = Number(prompt("Enter a 2nc Number"))
+// if(a>b){
+//     console.log(a + "is greatest");
+    
+// }else{
+//     console.log(b + " is greatest");
+    
+// }
+
+console.log("Accept an integar and check whether it is even or odd?")
+// Accept an integar and check whether it is even or odd?
+// let even = Number(prompt("Enter a number"))
+// if( even % 2 == 0 ){
+//     console.log("even");
+    
+// }else{
+//     console.log("odd");
+    
+// }
+
+console.log("Accept Name and Age form the user. Check if user is valid voter or not?");
+// Accept Name and Age form the user. Check if user is valid voter or not?
+// let username = String(prompt("Enter your Name? "))
+// let age = Number(prompt("Enter your Age? "))
+
+// if(age>=18){
+//     console.log("Valid Voter ");
+    
+// }else{
+//     console.log("Not Valid");
+    
+// }
