6dec951b0a1a8c1cc2be69b3795704062a879132
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-03-20 05:04:51 -0400

    Add files via upload

---
+++ Day 4/script.js
@@ -0,0 +1,106 @@
+console.log("Q. Accept an integer and Print hello world n times");
+// // Q. Accept an integer and Print hello world n times
+
+// // let n = Number(prompt("Enter a number? "))
+// // if(isNaN(n)){
+// //     console.log("Invalid Input");
+// // }else{
+// //     for(let i=1; i<=n; i++){
+// //         console.log("Hello World");
+        
+// //     }
+// //     console.log("Fail at"+ i);
+    
+// // }
+
+console.log("Q. Print natural number up to n.");
+// // Q. Print natural number up to n.
+
+// // let n =Number(prompt("Enter a number"))
+// // for(let i =1 ; i<=n; i++){
+// //     console.log(i);
+    
+// // }
+
+
+// console.log("Q. Reverse for loop. Print n to 1..");
+// // Q. Reverse for loop. Print n to 1.
+// // let n =Number(prompt("Enter a number"))
+// // for(let i =n ; i>=1; i--){
+// //     console.log(i);
+    
+// // }
+
+
+console.log("Q. Take a number as input and print its table    5 * 1 = 5     5 * 2 = 10 ... up to 10 terms");
+// Q. Take a number as input and print its table
+// 5 * 1 = 5
+// 5 * 2 = 10 ... up to 10 terms
+
+// let n = Number(prompt("Enter a Number"))
+
+// for(let i = 1; i<=10; i++){
+//     console.log(n +" * " + i + " = " + (n * i));
+    
+// }
+
+console.log("Q. Sum up to n terms.");
+// Q. Sum up to n terms.
+
+// let n = Number(prompt("Enter a Value"))
+// sam = 0;
+
+// for(let i =1; i<=n; i++){
+//     sam += i;
+// }
+// console.log(sam);
+
+
+console.log("Q. Factorial of a number");
+// Q. Factorial of a number
+// let n = Number(prompt("Enter a Value"))
+// Factorial = 1;
+
+// for(let i =1; i<=n; i++){
+//     Factorial *= i;   
+// }
+// console.log(Factorial);
+
+console.log("Q. Print the sum of all even & odd numbers in a range seperately.");
+//  Q. Print the sum of all even & odd numbers in a range seperately.
+// let n = Number(prompt("Enter a number"))
+// let evenSum = 0;
+// let oddSum =0;
+
+// for(let i=1; i<=n;i++){
+//     if(i%2==0) evenSum = evenSum + i;
+//     else oddSum = oddSum +i;
+// }
+
+// console.log("Even Sum is "+evenSum);
+// console.log("Odd Sum is "+oddSum);
+
+console.log("Q. Print all the factors of a number.");
+// Q. Print all the factors of a number.
+// let n = Number(prompt("Enter a Number"))
+// for(let i = 1; i<=n/2; i++){
+//     if(n % i ==0) console.log(i);
+    
+// }
+// console.log(n);
+
+console.log("Q. Check if the number is Prime or not.");
+// Q. Check if the number is Prime or not.
+
+// let n = Number(prompt("Enter a Number"));
+// let isPrime = true;
+
+// for(let i = 2; i<n; i++){
+//     if(n%i ==0){
+//         isPrime=false
+//         break
+//     }
+// }
+// if(isPrime)console.log("Prime Number");
+// else console.log("Not a Prime Number");
+
