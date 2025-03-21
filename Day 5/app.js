29a52cfa4afc5ec8bae28c7597be4255bf216827
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-03-21 05:34:21 -0400

    Add files via upload

---
+++ Day 5/app.js
@@ -0,0 +1,50 @@
+// ## —while loop ( Q.sum of digit )
+
+// let n = Number(prompt("Enter a number"))
+// sum = 0;
+
+// while(n>0){
+//     let rem = n%10;
+//     sum = sum + rem;
+//     n = Math.floor(n/10);
+// }
+// console.log(sum);
+
+console.log("Q.Automorphic number");
+// Q.Automorphic number
+
+// let n = Number(prompt("Enter a Number"));
+// let copy = n;
+// let sq = n*n;
+// let count = 0;
+
+// while(n>0){
+//     count++;
+//     n = Math.floor(n/10);
+// }
+// if(sq%Math.pow(10, count)==copy) console.log("Automorephic Number");
+// else console.log("Not a Automorephic Numbe");
+
+
+console.log("Question. What will be the output of the following code?");
+
+// let i = 3;
+// while (i > 0) {
+// console. log (i) ;
+// }
+
+console.log("What will be the output of this code?");
+ 
+// let x = 5;
+// while(x){
+//     console.log(x);
+//     x--;
+    
+// }
+
+
+let i = 0;
+while(++i<5){
+    console.log(i);
+    
+}
