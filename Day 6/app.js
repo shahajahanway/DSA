ac8c13f0b7fc351731b1d8a2a78d0a7b119a19c2
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-03-24 12:12:27 -0400

    Add files via upload

---
+++ Day 6/app.js
@@ -0,0 +1,30 @@
+// let i =1;
+// do{
+//     console.log("Hello World");
+//     i++;
+// }while(i<=5);
+
+console.log("—do-while Q. Repeat hello");
+// Q. Repeat hello
+
+// let userInput;
+// do{
+//     console.log("Hello");
+//     userInput=prompt("continue or Not?").toLocaleLowerCase()
+// }while(userInput ==="yes");
+
+console.log("Q. Guess the number");
+// Q. Guess the number
+
+
+// let com = Math.floor(Math.random()*100)+ 1;
+// let userInput;
+// do{
+//     userInput= Number(prompt("guess the number in b/w 1 too 100"));
+//     if(isNaN(userInput)|| userInput<0 || userInput > 100){
+//         console.log("Please enter a valid number");
+//         continue;
+//     }if (userInput >com)console.log("too high, try again");
+//     else if (userInput <com)console.log("too low, try again");
+//     else console.log("congrats and number was "+ com);
+// }while(userInput !== com);
