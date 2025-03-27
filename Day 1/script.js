cd292ceed27a5d20db8ffd72b4fd0874f9739a9c
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-03-27 14:27:09 -0400

    Add files via upload

---
+++ Day 1/script.js
@@ -0,0 +1,7 @@
+let h1 = document.querySelector("h1");
+let btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  h1.innerHTML = "Shahajahan";
+  h1.style.color = "lightgreen";
+  h1.style.backgroundColor = "blue";
+});
