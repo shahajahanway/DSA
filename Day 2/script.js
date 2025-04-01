25e71da9c4f6ea2df05663347f33881ecabdb523
Shahajahan Sheikh <167621740+shahajahanway@users.noreply.github.com>
2025-04-01 14:21:35 -0400

    Add files via upload

--- Day 2/script.js
+++ Day 2/script.js
@@ -1,78 +1,22 @@
-// Generate OTP ?
-// console.log(Math.floor(Math.random()*900000 + 100000));
+// Q1: Calculate Compound Interest
+// 💡 Use-Case: Loan Interest Calculation
+// 📌 Formula:
+// 𝐴=𝑃×(1+𝑟100)𝑡A=P×(1+ 100/r)t
 
-console.log("CalCulate Compound interest");
-// CalCulate Compound interest ?
-// let p = Number(prompt("Enter a price"))
-// let r = Number(prompt("Enter a rate"))
-// let t = Number(prompt("Enter a time "))
+function compoundInterest (P,r,t){
+    let A = P * Math.pow((1 + r / 100), t);
+    return Number(A.toFixed(2));
+}
+console.log(compoundInterest(1000, 5, 2)); 
+// Explanation:
+// Depositing ₹10,000 in a bank at 5% interest will result in a final amount of ₹11,025 after 2 years.
 
-// console.log(( p * Math.pow ( 1+ r/100, t)) - p );
+// 🔢 Q2: Generate a 6-Digit OTP
+// 💡 Use-Case: Secure Login Authentication
 
-console.log("Area of triangle of heron's formula");
-
-// Area of triangle of heron's formula
-// let a = Number(prompt("Enter a number"))
-// let b = Number(prompt("Enter a 2nd number"))
-// let c = Number(prompt("Enter a 3td number"))
-
-// if (a+b <= c || a+c <= b || b+c <= a ) {
-//     console.log("Not Possiable");
-    
-// }else{
-//     let s = (a+b+c)/2
-//     console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
-    
-// }
-
-console.log("circumference of circle?");
-// circumference of circle?
-// let r = Number(prompt("Enter a number"))
-// console.log(2*Math.PI * r);
-
-
-console.log("If- Else");
-// If - Else
-// if(10>37){
-//     console.log("hello world");
-// }else if(18>5){
-//     console.log("world");
-// }else{
-//     console.log("Hello Worlds ways");
-    
-// }
-console.log("Accept two number and print the greates between them?");
-// Accept two number and print the greates between them?
-// let a = Number(prompt("Enter a Number"))
-// let b = Number(prompt("Enter a 2nc Number"))
-// if(a>b){
-//     console.log(a + "is greatest");
-    
-// }else{
-//     console.log(b + " is greatest");
-    
-// }
-
-console.log("Accept an integar and check whether it is even or odd?")
-// Accept an integar and check whether it is even or odd?
-// let even = Number(prompt("Enter a number"))
-// if( even % 2 == 0 ){
-//     console.log("even");
-    
-// }else{
-//     console.log("odd");
-    
-// }
-
-console.log("Accept Name and Age form the user. Check if user is valid voter or not?");
-// Accept Name and Age form the user. Check if user is valid voter or not?
-// let username = String(prompt("Enter your Name? "))
-// let age = Number(prompt("Enter your Age? "))
-
-// if(age>=18){
-//     console.log("Valid Voter ");
-    
-// }else{
-//     console.log("Not Valid");
-    
-// }
+function generateOTP() {
+    return Math.floor(100000 + Math.random() * 900000);
+}
+console.log(generateOTP());  
+// Explanation:
+// To generate an OTP, we use Math.random() which returns a 6-digit random number.
